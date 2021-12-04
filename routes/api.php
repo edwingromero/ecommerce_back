5e6ee7e67963f600e8e092b61ca7a318aee1bd3b
Edwing Romero <edwing8957@gmail.com>
2021-12-04 01:31:18 -0500

    implementar JWT

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+Route::post('login', [AuthController::class,'login'])->name('login');
+Route::post('refresh', [AuthController::class,'refresh']);
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('logout', [AuthController::class,'logout']);
+    Route::post('me', [AuthController::class,'me']);
+
+    Route::resource('cursos',CursoController::class);
+
+});
+
+

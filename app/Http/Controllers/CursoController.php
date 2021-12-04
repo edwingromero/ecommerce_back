54c034d1d49e673d95f5a42c7dc6ae89dac3eb8f
Edwing Romero <edwing8957@gmail.com>
2021-12-04 01:32:58 -0500

    api rest curso

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Curso;
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $curso = Curso::with(['autor','avgCalificacion'])->get();
+        return response($curso);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $curso = new Curso;
+        $curso->nombre = $request->nombre;
+        $curso->resumen = $request->resumen;
+        $curso->autor_id = $request->autor_id;
+        $curso->save();
+        return response($curso);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $curso = Curso::find($id);
+        $curso->nombre = $request->nombre;
+        $curso->resumen = $request->resumen;
+        $curso->autor_id = $request->autor_id;
+        $curso->save();
+        return response($curso);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $curso = Curso::find($id);
+        $curso->delete();
+        return response($curso);
+    }
+}
--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -10,4 +10,42 @@ class Curso extends Model
     use HasFactory;
 
     protected $table = 'cursos';
+
+    public function autor()
+    {
+        return $this->hasOne(Autor::class,'id','autor_id');
+    }
+
+    public function getImagenAttribute($value)
+    {
+        if ($value) {
+            return asset('storage/images/' . $value);
+        } else {
+            return asset('images/profile/no-image.png');
+        }
+    }
+
+    public function reviewRows()
+    {
+        return $this->hasMany(Calificacion::class);
+    }
+
+
+    public function avgCalificacion()
+    {
+        return $this->reviewRows()
+        ->selectRaw('avg(puntaje) as promedio, curso_id')
+        ->groupBy('curso_id');
+    }
+
+    public function getAvgCalificacionAttribute()
+    {
+        if ( ! array_key_exists('avgCalificacion', $this->relations)) {
+            $this->load('avgCalificacion');
+        }
+
+        $relation = $this->getRelation('avgCalificacion')->first();
+
+        return ($relation) ? $relation->aggregate : null;
+    }
 }

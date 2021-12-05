7eab5b3a6f2f4f4711a93500b679f9d8886ca533
Edwing Romero <edwing8957@gmail.com>
2021-12-05 00:11:57 -0500

    customizar mensaje de error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,11 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Response;
+use Exception;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -34,8 +39,27 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
+        /*$this->reportable(function (Throwable $e) {
             //
+        });*/
+        
+        $this->renderable(function(TokenInvalidException $e, $request){
+            return Response::json([
+                'error' => 'Invalid token'
+            ],401);
         });
+        $this->renderable(function (TokenExpiredException $e, $request) {
+            return Response::json([
+                'error' => 'Token not Expired'
+            ],401);
+
+        });
+
+        $this->renderable(function (JWTException $e, $request) {
+            return Response::json([
+                'error' => 'Token not parsed'
+            ],401);
+        });
+        
     }
 }
